96b786254b9d244a92a9775897e58513a10591d2
Avexy <jackosbornlot@gmail.com>
2026-04-23 17:07:04 -0400

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -1,1 +1,3 @@
 // please visit this go file for updated offsets, because its like 36MB and github only allows 25MB: https://gofile.io/d/SorYch
+
+// if the hpp file is not opening then try opening it in a IDE to view the file.
